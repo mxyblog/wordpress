bc27cdadb217adec1be69d7a4b67f648a49a4814
xiangyongjun <274728691@qq.com>
2019-09-24 12:01:18 +0800

    fix title

--- yusi2019/header.php
+++ yusi2019/header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=10,IE=9,IE=8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-<title><?php wp_title('-', true, 'right'); echo get_option('blogname'); if (is_home ()) echo get_option('blogdescription'); if ($paged > 1) echo '-Page ', $paged; ?></title>
+<title><?php echo get_option('blogname'); if (is_home ()) echo '-'.get_option('blogdescription'); if ($paged > 1) echo '-Page ', $paged; ?></title>
 <?php
 $sr_1 = 0; $sr_2 = 0; $commenton = 0; 
 if( dopt('d_sideroll_b') ){ 
